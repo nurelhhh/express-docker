a5dd3456a3d222612adaff14585b299d90d8f3ac
nurelhhh <nurel_harsya@outlook.com>
2022-03-09 12:05:43 +0700

    Install jest and update Dockerfile

--- app/server.js
+++ app/server.js
@@ -1,15 +1,17 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
 // Constants
-const PORT = 8080;
+const PORT = 3000;
 const HOST = '0.0.0.0';
 
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+//   res.send('Hello World');
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(PORT, HOST);
